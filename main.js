9832ed5190a0beb9357a9bc8ce897f0438be8a77
Boso Mac <robertoingrao4@gmail.com>
2026-01-18 09:30:57 +0100

    selfwork js 1

---
+++ main.js
@@ -0,0 +1,14 @@
+let num1 = 5;
+let num2 = 10;
+let stringa = 'Ciao sono una stringa';
+const PIGRECO = 3.14159265359;
+
+console.log(num1);
+console.log(num2);
+console.log(stringa);
+
+stringa = ( num1 + num2 );
+console.log(stringa);
+
+console.log(PIGRECO);
+PIGRECO=(33);
